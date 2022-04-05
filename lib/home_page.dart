b0d99ca46a424ca538fab78dc3e62bd179be4888
bruno.noveli <brunimzuppwolf@gmail.com>
2022-04-05 10:28:02 -0300

    Exemplo de execuções do sqlite

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,6 +18,29 @@ class _HomePageState extends State<HomePage> {
     DatabaseSqlite().openConnection();
   }
 
+  Future<void> _database() async {
+
+    // final database = await DatabaseSqlite().openConnection();
+
+    // database.insert("teste", {"nome" : "Bruno Noveli"});
+    // database.delete("teste", where: "nome = ?", whereArgs: ["Bruno Noveli"]);
+    // database.update(
+    //   "teste", {
+    //     "nome": "Academia do Flutter"
+    //   },
+    //   where: "nome = ?",
+    //   whereArgs: ["Bruno Noveli"]
+    // );
+    // var result = await database.query("teste");
+    // print(result);
+
+    // database.rawInsert("INSERT INTO teste VALUES(NULL, ?)", ["Bruno"]);
+    // database.rawUpdate("UPDATE teste SET nome = ? where id = ?", ["Bruno Academia do Flutter", 5]);
+    // database.rawDelete("DELETE FROM teste WHERE id = ?", [5]);
+    // var result2 = await database.rawQuery("SELECT * FROM teste");
+    // print(result2);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
